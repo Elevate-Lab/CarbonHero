f2f3d1e5d828e7592b52328e5b9ae63c27b852c6
Pranshu-Tripathi <mtpranshu2001@gmail.com>
2021-03-09 01:30:01 +0530

    providers added

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class User with ChangeNotifier{
+class User with ChangeNotifier {
   String userId;
   String userName;
   String emailId;
@@ -29,13 +29,111 @@ class User with ChangeNotifier{
   //   @required this.userFriends,
   // });
 
-  set f(String name){
-    this.userName = name;
+  set name(String _username) {
+    this.userName = _username;
     notifyListeners();
   }
+
+  set id(String _userId) {
+    this.userId = _userId;
+    notifyListeners();
+  }
+
+  set email_id(String _useremailId) {
+    this.emailId = _useremailId;
+    notifyListeners();
+  }
+
+  set img_url(String _imgUrl) {
+    this.imgUrl = _imgUrl;
+    notifyListeners();
+  }
+
+  set saved_carbon_emission(double _savedcarbonemission) {
+    this.savedCarbonEmission = _savedcarbonemission;
+    notifyListeners();
+  }
+
+  set total_carbon_emission_this_month(double _totalcarbonemissionthismonth) {
+    this.totalCarbonEmissionThisMonth = _totalcarbonemissionthismonth;
+    notifyListeners();
+  }
+
+  set total_carbon_emission_last_month(double _totalcarbonemissionlastmonth) {
+    this.totalCarbonEmissionThisMonth = _totalcarbonemissionlastmonth;
+    notifyListeners();
+  }
+
+  set total_carbon_emission_today(double _totalcarbonemissiontoday) {
+    this.totalCarbonEmissionToday = _totalcarbonemissiontoday;
+    notifyListeners();
+  }
+
+  set total_carbon_emission_yesterday(double _totalcarbonemissionyesterday) {
+    this.totalCarbonEmissionYesterday = _totalcarbonemissionyesterday;
+    notifyListeners();
+  }
+
+  set points_earned(int _pointsearned) {
+    this.pointsEarned = _pointsearned;
+    notifyListeners();
+  }
+
+  set batches_earned(List<dynamic> _batchesearned) {
+    this.batchesEarned = _batchesearned;
+    notifyListeners();
+  }
+
+  set user_friends(List<dynamic> _userfriends) {
+    this.userFriends = _userfriends;
+    notifyListeners();
+  }
+
   String get name {
-    print("679t");
-    print(this.userName);
     return this.userName;
+  }
+
+  String get id {
+    return this.userId;
+  }
+
+  String get email_id {
+    return this.emailId;
+  }
+
+  String get img_url {
+    return this.imgUrl;
+  }
+
+  double get saved_carbon_emission {
+    return this.savedCarbonEmission;
+  }
+
+  double get total_carbon_emission_last_month {
+    return this.totalCarbonEmissionLastMonth;
+  }
+
+  double get total_carbon_emission_this_month {
+    return this.totalCarbonEmissionThisMonth;
+  }
+
+  double get total_carbon_emission_today {
+    return this.totalCarbonEmissionToday;
+  }
+
+  double get total_carbon_emission_yesterday {
+    return this.totalCarbonEmissionYesterday;
+  }
+
+  int get points_earned {
+    return this.pointsEarned;
+  }
+
+  List<dynamic> get batches_earned {
+    return this.batchesEarned;
+  }
+
+  List<dynamic> get user_friends {
+    return this.user_friends;
   }
 }

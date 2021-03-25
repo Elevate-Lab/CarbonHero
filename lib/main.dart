752fae4860bc486f125099b3f1801fcd76f7d480
Abhijeet-sonkar <abhijeet.iiita.iib@gmail.com>
2021-03-25 12:10:32 +0530

    fixes activity card

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyApp extends StatelessWidget {
           primaryColor: const Color(0xff281627),
           accentColor: const Color(0xffFEBB46),
         ),
-        initialRoute: chooseNewActivity.routeName,
+        initialRoute: SplashScreen1.routeName,
         routes: {
           HomeScreen.routeName: (ctx) => HomeScreen(),
           SplashScreen1.routeName: (ctx) => SplashScreen1(),
